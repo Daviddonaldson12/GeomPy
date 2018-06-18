defdb80480196b27602a41daf76cdf4f7ba87c3c
Daviddonaldson12 <39635182+Daviddonaldson12@users.noreply.github.com>
2018-06-18 18:49:32 -0400

    Add Fermat Test
    I'm going to start adding more throughout this week, if you have any suggestions feel free to let me know on discord.

--- geompy/general/primality.py
+++ geompy/general/primality.py
@@ -3,3 +3,13 @@
 Implementations of various primality tests
 
 """
+
+def fermatTest(number):
+    
+    if pow(2, number - 1, number) == 1:
+        return str(number) + " is Probably Prime."
+    if pow(2, number - 1, number) != 1:
+        return str(number) + " is Certainly Composite."
+
+
+
